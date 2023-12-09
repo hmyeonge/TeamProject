075a832224b5baa639473090fbb0a5a85962a067
장해명 <hmyeonge@gmail.com>
2023-12-09 18:37:19 +0900

    몸무게 DB 레이아웃
    - 일지 화면 레이아웃 구현
    - 몸무게, 날짜 항목 레이아웃 구현
    - 팝업창 레이아웃 구현

---
+++ app/src/main/java/com/petcare/newteamproject/RecordWeightActivity.java
@@ -0,0 +1,14 @@
+package com.petcare.newteamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordWeightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_weight);
+    }
+}
