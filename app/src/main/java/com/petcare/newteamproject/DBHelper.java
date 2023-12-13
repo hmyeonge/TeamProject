79dfde84146a6bf31278856bc347a2ecb765a3fe
장해명 <hmyeonge@gmail.com>
2023-12-14 08:23:14 +0900

    산책 시간 저장 기능, 체중DB와 userID 연결 준비
    - 산책 시간을 저장하는 기능 구현
    - MainActivity.java 에서 이후 userID 와 연결할 때 필요한 코드 주석으로 적어둠
    - MainActivity,java 에서 체중 DB와 산책 DB 모두 user 별로 다르게 저장될 수 있도록 필요한 코드를 주석으로 적어둔 것

--- app/src/main/java/com/petcare/newteamproject/DBHelper.java
+++ app/src/main/java/com/petcare/newteamproject/DBHelper.java
@@ -13,40 +13,41 @@ import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    // 클래스 외부에서는 호출을 못하도록 private 로 지정
-    private static final int DB_VERSION = 2; // DB 버전 지정
+    private static final int DB_VERSION = 3; // DB 버전 지정
     private static final String DB_NAME = "pet-weight.db"; // DB 이름 지정
 
     public DBHelper(@Nullable Context context) {
         super(context, DB_NAME, null, DB_VERSION);
     }
 
-    // petWeight, petWalk 테이블 생성
     @Override
     public void onCreate(SQLiteDatabase db) {
         // 데이터베이스가 생성될 때 호출되어 테이블이 없으면 테이블 생성
-        // petWeight 테이블 : id , weight , writeDate 컬럼을 가짐 (아래에서 userID 컬럼 추가)
+        // petWeight 테이블 : id , weight , writeDate 컬럼을 가짐 (이후 userID 컬럼 추가)
         // petWalk 테이블 : id , userID , time , writeDate 컬럼을 가짐
         db.execSQL("CREATE TABLE IF NOT EXISTS petWeight (id INTEGER PRIMARY KEY AUTOINCREMENT, weight REAL NOT NULL, writeDate TEXT NOT NULL)");
         db.execSQL("CREATE TABLE IF NOT EXISTS petWalk (id INTEGER PRIMARY KEY AUTOINCREMENT , userID TEXT NOT NULL, time TEXT NOT NULL , writeDate TEXT NOT NULL)");
     }
 
-    // DB 업그레이드
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        if (oldVersion < newVersion) {
-            // 버전에 따라 다양한 업그레이드 작업을 수행
-            switch (oldVersion) {
-                case 1:
-                    // petWeight 테이블에 userID 컬럼 추가
-                    db.execSQL("ALTER TABLE petWeight ADD COLUMN userID INTEGER NOT NULL DEFAULT 1");
-                case 2:
-                    // 이후 업그레이드 작업을 수행
-            }
+        if (oldVersion < 2) {
+            // 임시 테이블 생성
+            db.execSQL("CREATE TABLE temp_petWeight (id INTEGER PRIMARY KEY AUTOINCREMENT, userID TEXT NOT NULL, weight REAL NOT NULL, writeDate TEXT NOT NULL)");
+
+            // 기존 데이터를 임시 테이블로 복사
+            // 기존 userID 가 INTEGER 로 설정되어 있으므로 이를 문자열로 변환
+            db.execSQL("INSERT INTO temp_petWeight (id, userID, weight, writeDate) SELECT id, CAST(userID AS TEXT), weight, writeDate FROM petWeight");
+
+            // 기존 테이블 삭제
+            db.execSQL("DROP TABLE petWeight");
+
+            // 임시 테이블 이름 변경
+            db.execSQL("ALTER TABLE temp_petWeight RENAME TO petWeight");
         }
+        // 추가 업그레이드 작업
     }
 
-    // 체중 데이터베이스의 SELECT 문과 INSERT 문
     // SELECT (체중 데이터 조회)
     public ArrayList<WeightItem> get_petWeight() {
       ArrayList<WeightItem> weightItems = new ArrayList<>();
@@ -66,15 +67,17 @@ public class DBHelper extends SQLiteOpenHelper {
               weightItems.add(weightItem);
           }
        }
-      cursor.close();
+      cursor.close(); // Cursor 객체를 사용해 쿼리 결과를 처리, close( ) 메소드 호출로 리소스 해제
       return weightItems;
     }
 
     // INSERT (체중 데이터를 DB에 삽입)
-    public void InsertWeight(Double _weight, String _writeDate){
+    // TODO : userID
+    public void InsertWeight(String userID, Double weight, String writeDate){
         SQLiteDatabase db = getWritableDatabase();
-        db.execSQL("INSERT INTO petWeight(weight, writeDate) VALUES ('"+ _weight + "' , '"+ _writeDate + "')");
+        db.execSQL("INSERT INTO petWeight(userID, weight, writeDate) VALUES ('" + userID + "', " + weight + " , '" + writeDate + "')");
     }
+
 
     // 산책 데이터베이스의 SELECT 문과 INSERT 문
     // SELECT (산책 데이터 조회)
@@ -94,13 +97,13 @@ public class DBHelper extends SQLiteOpenHelper {
                 walkItems.add(walkItem);
             }
         }
-        cursor.close();
+        cursor.close(); // Cursor 객체를 사용해 쿼리 결과를 처리, close( ) 메소드 호출로 리소스 해제
         return walkItems;
     }
 
     // INSERT (산책 데이터 삽입)
     public void InsertWalk(String userID, String time, String writeDate){
-        SQLiteDatabase db = getWritableDatabase();
+        SQLiteDatabase db = getWritableDatabase(); // SQLiteDatabase 객체 가져옴
         db.execSQL("INSERT INTO petWalk (userID, time, writeDate) VALUES ('" + userID + "', '" + time + "', '" + writeDate + "')");
     }
 
--- app/src/main/java/com/petcare/newteamproject/MainActivity.java
+++ app/src/main/java/com/petcare/newteamproject/MainActivity.java
@@ -1,7 +1,6 @@
 package com.petcare.newteamproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,7 +12,6 @@ import android.widget.Chronometer;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.Locale;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -23,12 +21,9 @@ import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
     private Chronometer chronometer;
-    private Button startButton;
-    private Button stopButton;
-    private Button saveWeightButton;
     private EditText weightInput;
+    private long startTime;
     private DBHelper dbHelper;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,52 +31,60 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // 위젯 참조
-        chronometer = findViewById(R.id.chronometer);
-        startButton = findViewById(R.id.start_walk_button);
-        stopButton = findViewById(R.id.stop_walk_button);
-        saveWeightButton = findViewById(R.id.save_weight_button);
-        weightInput = findViewById(R.id.weight_input);
-
         Button recordWeightButton = findViewById(R.id.record_weight_button);
         Button recordWalkButton = findViewById((R.id.record_walk_button));
+        Button startButton = findViewById(R.id.start_walk_button);
+        Button stopButton = findViewById(R.id.stop_walk_button);
+        Button saveWeightButton = findViewById(R.id.save_weight_button);
+        weightInput = findViewById(R.id.weight_input);
+        chronometer = findViewById(R.id.chronometer);
 
         // DBHelper 인스턴스화
         dbHelper = new DBHelper(this);
 
+        /* TODO : 로그인한 사용자의 userID 가져와야함
+         * SharedPreferences prefs = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+         * String loggedInUserID = prefs.getString("UserID", "defaultUserID");
+         *
+         * SharedPreferences 를 사용해 로그인한 사용자의 userID 가져옴
+         * UserPrefs :  사용자의 설정을 저장하는 파일의 이름
+         * getString 메소드 : 'UserID' 키에 해당하는 값을 반환, 값이 없는 경우 defaultUserID 를 기본값으로 반환
+         */
 
-        // 체중 저장 버튼 리스너 설정
+        // 체중저장 버튼 리스너
         saveWeightButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            // 체중 저장
             public void onClick(View v) {
+                // TODO -> saveWeight(loggedInUserID);
                 saveWeight();
             }
         });
 
-        // 체중 일지 버튼 리스너 설정
+        // 체중일지 버튼 리스너
         recordWeightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 인텐트를 사용해 RecordWeightActivity 라는 새 액티비티 시작
+                // 인텐트를 사용해 RecordWeightActivity 시작
                 Intent intent = new Intent(MainActivity.this, RecordWeightActivity.class);
                 startActivity(intent);
             }
         });
 
-        // 산책 시작 버튼 리스너 설정
+        // 산책시작 버튼 리스너
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                chronometer.setBase(SystemClock.elapsedRealtime()); // 타이머 초기화
-                chronometer.start(); // 타이머 시작
+                startTime = SystemClock.elapsedRealtime(); // startTime 변수를 초기화
+                chronometer.setBase(SystemClock.elapsedRealtime()); // 스톱워치가 현재 시점을 시작점으로 시간 계산 시작
+                chronometer.start(); // 스톱워치 시작
             }
         });
 
-        // 산책 종료 버튼 리스너 설정
+        // 산책종료 버튼 리스너
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                chronometer.stop(); // 타이머 중지
+                saveWalk();
             }
         });
 
@@ -97,30 +100,32 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    // 체중 저장 : saveWeight 메소드 사용해 사용자가 입력한 체중 검증
+    // saveWeight 메소드 : 체중 데이터를 데이터베이스에 저장, TODO : 로그인한 사용자의 userID 가져오기 -> private void saveWeight(String userID)
     private void saveWeight(){
+
         // EditText 에서 입력된 체중 값을 문자열로 가져옴
         String weightStr = weightInput.getText().toString();
 
         // 입력된 문자열이 비어있지 않은지 확인
         if(!weightStr.isEmpty()) {
             try {
-                Toast.makeText(MainActivity.this, "체중값이 저장되었습니다", Toast.LENGTH_SHORT).show();
 
                 // 문자열을 double 타입으로 변환해 체중 값으로 사용
                 double weight = Double.parseDouble(weightStr);
 
-                // 현재 날짜를 문자열로 가져오는 메소드 호출
-                String currentDate = getCurrentDate();
+                String currentDate = getCurrentDate(); // 현재 날짜를 문자열로 가져오는 메소드 호출
+                Log.d("Current Date", currentDate); /// 현재 날짜를 로그로 출력
 
-                // 로그로 현재 날짜 출력
-                Log.d("Current Date", currentDate);
+                // 임시 사용자 ID 설정, TODO : 이후 삭제
+                String tempUserId = "tempUser";
 
-                // DBHelper 인스턴스로 데이터베이스에 체중이랑 날짜 저장
-                dbHelper.InsertWeight(weight, currentDate);
+                // 체중 데이터를 데이터베이스에 저장, TODO -> dbHelper.InsertWeight(userID, weight, currentDate);
+                dbHelper.InsertWeight(tempUserId, weight, currentDate);
+
+                Toast.makeText(MainActivity.this, "체중값이 저장되었습니다", Toast.LENGTH_SHORT).show();
 
             } catch (NumberFormatException e) {
-                // EditText 에 숫자를 입력하지 않는 경우 토스트 메시지를 띄움
+                // 몸무게 입력란에 숫자를 입력하지 않는 경우 토스트 메시지를 띄움
                 Toast.makeText(MainActivity.this, "유효한 숫자를 입력하세요", Toast.LENGTH_SHORT).show();
             }
         } else {
@@ -129,11 +134,35 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // saveWalk 메소드 : 산책시간 데이터를 데이터베이스에 저장, TODO : 로그인한 사용자의 userID 가져와야함 -> private void saveWalk(String userID)
+    private void saveWalk() {
+        long duration = SystemClock.elapsedRealtime() - startTime; // 스톱워치 지속시간 계산
+        String formattedDuration = formatDuration(duration); // 지속시간을 문자열로 변환
+        chronometer.stop(); // 타이머 중지
+
+        String currentDate = getCurrentDate(); // // 현재 날짜를 문자열로 가져오는 메소드 호출
+        Log.d("Current Date", currentDate); // 현재 날짜를 로그로 출력
+
+        // 임시 사용자 ID 설정, TODO : 이후 삭제
+        String tempUserId = "tempUser";
+
+        // 산책시간 데이터를 데이터베이스에 저장, TODO -> dbHelper.InsertWalk(userID, formattedDuration, currentDate);
+        dbHelper.InsertWalk(tempUserId, formattedDuration, currentDate);
+    }
+
     // 현재 날짜를 yyyy-mm-dd 형식 문자열로 변환
     private String getCurrentDate() {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.KOREA);
         dateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
         return dateFormat.format(new Date());
+    }
+
+    // 스톱워치 지속시간을 문자열로 변환
+    private String formatDuration(long duration) {
+        int seconds = (int) (duration / 1000) % 60 ;
+        int minutes = (int) ((duration / (1000*60)) % 60);
+        int hours   = (int) ((duration / (1000*60*60)) % 24);
+        return String.format(Locale.KOREA, "%02d:%02d:%02d", hours, minutes, seconds); // 한국 로케일로 설정
     }
 
 }
